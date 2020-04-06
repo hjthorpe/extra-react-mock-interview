a770da9f3739657ceacdb32396f18d591b729434
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-06 12:13:40 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
@@ -7,14 +11,17 @@ import './App.css'
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <h2>Hello World</h2>
-        <HelloWorld />
+         <Route path='/' component={HelloWorld} />
         <h2>Bomb</h2>
-        <Bomb />
+        <Route path='/' component={Bomb} />
         <h2>Roulette Gun</h2>
-        <RouletteGun bulletInChamber={6} />
+        <Route path='/' component={RouletteGun} />
       </div>
+      </BrowserRouter>
+
     );
   }
 }
